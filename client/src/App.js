c9b226719fd1410c5f720bc72f6790cdcd0e125c
Shivam Vishwakarma <84992044+Shivam-vishwakarma991@users.noreply.github.com>
2023-04-14 08:58:50 +0530

    metamask integration

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import DAO from "./contracts/DAO.json";
 import "./App.css";
 import Investors from "./components/Investors/Investors";
 import Manager from "./components/Manager/Manager";
+import detectEthereumProvider from "@metamask/detect-provider";
 
 function App() {
   const [state, setState] = useState({
@@ -11,19 +12,25 @@ function App() {
     contract: null,
   });
   const [account, setAccount] = useState("Not connected");
-  const [balance,setBalance]= useState(0)
+  const [balance, setBalance] = useState(0);
   useEffect(() => {
     async function init() {
-      const provider = new Web3.providers.HttpProvider("HTTP://127.0.0.1:7545");
+      // const provider = new Web3.providers.HttpProvider("HTTP://127.0.0.1:7545");
+      const provider = await detectEthereumProvider();
       const web3 = new Web3(provider);
+      console.log(web3)
       const networkId = await web3.eth.net.getId();
       const deployedNetwork = DAO.networks[networkId];
-      const contract = new web3.eth.Contract(
-       DAO.abi,
-        deployedNetwork.address
-      );
-     
-      setState({ web3: web3, contract: contract });
+      // console.log(networkId)
+      // console.log(deployedNetwork)
+      const contract = new web3.eth.Contract(DAO.abi, deployedNetwork.address);
+      console.log(contract)
+      if (provider) {
+        provider.request({ method: "eth_requestAccounts" });
+        setState({ web3: web3, contract: contract });
+      } else {
+        console.error("Please install MetaMask");
+      }
     }
     init();
   }, []);
@@ -53,50 +60,60 @@ function App() {
       selectedAccountAddress !== "Choose an account"
     ) {
       setAccount(selectedAccountAddress);
-    }
-    else{
-      setAccount("Not Connected")
+    } else {
+      setAccount("Not Connected");
     }
   };
 
-  useEffect(()=>{
-
+  useEffect(() => {
     const { web3 } = state;
-    const balanceSET= async ()=>{
-      if(account!=="Not connected"  ){
-        const fetchedBalance= await web3.eth.getBalance(account);
-        const convert=  web3.utils.fromWei(fetchedBalance,"ether")
+    const balanceSET = async () => {
+      if (account !== "Not connected") {
+        const fetchedBalance = await web3.eth.getBalance(account);
+        const convert = web3.utils.fromWei(fetchedBalance, "ether");
         // console.log(convert);
-        setBalance(convert)
-      }
-
-      else{
+        setBalance(convert);
+      } else {
         setBalance(0);
       }
+    };
+    web3 && balanceSET();
+  }, [state, account]);
 
-    }
-  web3 &&  balanceSET()
-  },[state,account])
-
-
-  
-//code for account balance
+  //code for account balance
   return (
     <div className="App">
-   <h1>Decentralize Autonoumous Organization</h1>
-   <p >Connected Account: {account}</p>
-   <p >Available Funds: {balance} ETH</p>
-   <form className="label0" id="myForm">
+      <h1>Decentralize Autonoumous Organization</h1>
+      <p>Connected Account: {account}</p>
+      <p>Available Funds: {balance} ETH</p>
+      <form className="label0" id="myForm">
         <label htmlFor=""></label>
         <select className="innerBox" id="selectNumber" onChange={selectAccount}>
           <option align="center">Choose an account</option>
         </select>
       </form>
-      <p style={{ color: 'rgb(7, 7, 39)', textAlign: 'center', fontSize: '25px', fontWeight: 'bold' }} >For Manager</p>
+      <p
+        style={{
+          color: "rgb(7, 7, 39)",
+          textAlign: "center",
+          fontSize: "25px",
+          fontWeight: "bold",
+        }}
+      >
+        For Manager
+      </p>
       <Manager state={state} account={account}></Manager>
-      <p style={{ color: 'rgb(7, 7, 39)', textAlign: 'center', fontSize: '25px', fontWeight: 'bold' }} >FOR INVESTORS</p>
-     <Investors state={state} account={account}></Investors>
-    
+      <p
+        style={{
+          color: "rgb(7, 7, 39)",
+          textAlign: "center",
+          fontSize: "25px",
+          fontWeight: "bold",
+        }}
+      >
+        FOR INVESTORS
+      </p>
+      <Investors state={state} account={account}></Investors>
     </div>
   );
 }
